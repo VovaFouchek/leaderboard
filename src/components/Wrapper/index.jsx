278fce9b7008e256f1fc2cd66988b59096c8a168
Volodymyr Fouchek <vinvova555333@gmail.com>
2022-04-25 16:45:49 +0300

    Added Wrapper componenet

---
+++ src/components/Wrapper/index.jsx
@@ -0,0 +1,9 @@
+import s from './wrapper.module.scss'
+
+export const Wrapper = ({ children }) => {
+    return (
+        <div className={s.inner}>
+            {children}
+        </div>
+    )
+}
