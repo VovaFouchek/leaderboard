4ccc3d71f4183e90436ece29393f20f345303829
Volodymyr Fouchek <vinvova555333@gmail.com>
2022-04-27 11:55:23 +0300

    Add component to App

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { Wrapper } from './components/Wrapper';
+import { Header } from './components/Header';
+import { Board } from './components/Board';
+
+
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Wrapper>
+        <Header />
+        <Board />
+      </Wrapper>
+    </div>
+  );
+}
+
+export default App;
