3f2ddfffe7bb0ffe3bdc57447e2a834d2aa5e6fb
Volodymyr Fouchek <vinvova555333@gmail.com>
2022-04-27 16:55:27 +0300

    Custom Header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,7 +1,20 @@
 import s from './header.module.scss'
+import image from '../../images/business-people.svg';
 
 export const Header = () => {
     return (
-        <h1 className={s.title}>LeaderBoard:</h1>
+        <>
+            <div className={s.banner}>
+                <p className={s.text}>All time Highest Scorers</p>
+                <div className={s.container__card}>
+                    <div className={s.card}>
+                        <img src="" alt="" />
+                        <p></p>
+                    </div>
+                </div>
+                <img src={image} alt="Business people" />
+            </div>
+            <h1 className={s.title}>LeaderBoard:</h1>
+        </>
     )
 }
