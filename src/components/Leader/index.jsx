4ba009be3b4fe965ff3bca7283488d79dba93609
Volodymyr Fouchek <vinvova555333@gmail.com>
2022-04-27 14:49:14 +0300

    Cheking on available score

--- src/components/Leader/index.jsx
+++ src/components/Leader/index.jsx
@@ -1,12 +1,12 @@
 import s from './leader.module.scss'
 
 export const Leader = ({ leader }) => {
-    console.log(leader);
+   // console.log(leader);
     return (
         <>
             <tr>
                 <td className={s.leader}>{leader.name}</td>
-                <td>{(leader.score >= 0) ? leader.score : ('No date')}</td>
+                <td className={s.score}>{(leader.score >= 0) ? leader.score : ('No date')}</td>
             </tr>
         </>
     )
