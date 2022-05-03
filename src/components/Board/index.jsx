de41af2ea26faaac7a22c1e798d12c8015553bdf
Volodymyr Fouchek <vinvova555333@gmail.com>
2022-05-03 16:14:14 +0300

    Added Position of Leaders

--- src/components/Board/index.jsx
+++ src/components/Board/index.jsx
@@ -6,6 +6,8 @@ import { useEffect, useState } from 'react';
 
 export const Board = () => {
     const [list, setList] = useState('');
+
+
     async function FetchData() {
         const response = await instance.get(axiosConfig.baseURL)
         setList(response.data);
@@ -15,21 +17,42 @@ export const Board = () => {
         FetchData();
     }, [])
     
+
+
+    function ordinal_suffix_of(i) {
+        let j = i % 10,
+            k = i % 100;
+        if (j === 1 && k !== 11) {
+            return i + "st";
+        }
+        if (j === 2 && k !== 12) {
+            return i + "nd";
+        }
+        if (j === 3 && k !== 13) {
+            return i + "rd";
+        }
+        return i + "th";
+    }
+    
+
     return (
+        <>
         <div className={s.board}>
             <table>
                 <thead>
                     <tr>
+                        <th>Position</th>
                         <th>Leader</th>
                         <th>Score</th>
                     </tr>
                 </thead>
                 <tbody>
-                    {list ? list.map((leader) => (
-                        <Leader leader={leader} key={leader.name} />
+                    {list ? list.map((leader, index) => (
+                        <Leader leader={leader} number={ordinal_suffix_of(index+1)} key={leader.name}/>
                     )) : <></>}
                 </tbody>
             </table>
         </div>
+        </>
     )
 }
--- src/components/Leader/index.jsx
+++ src/components/Leader/index.jsx
@@ -1,10 +1,11 @@
 import s from './leader.module.scss'
 
-export const Leader = ({ leader }) => {
-   // console.log(leader);
+export const Leader = ({ leader, number}) => {
+    console.log(leader);
     return (
         <>
             <tr>
+                <td className={s.position}>{number}</td>
                 <td className={s.leader}>{leader.name}</td>
                 <td className={s.score}>{(leader.score >= 0) ? leader.score : (0)}</td>
             </tr>
