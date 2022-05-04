0911b26e9f6d9bea6455cb5b14a0ea342b836a56
Volodymyr Fouchek <vinvova555333@gmail.com>
2022-05-04 15:44:27 +0300

    Added style for btn

--- src/components/Controls/index.jsx
+++ src/components/Controls/index.jsx
@@ -3,7 +3,10 @@ import s from './controls.module.scss'
 export const Controls = ({ list, setList, sortValue, setSortValue, sortedList }) => {
     return (
         <>
-            <button onClick={() => sortedList(list, setList, sortValue, setSortValue)}>Sorted by</button>
+            <div className={s.wrapControl}>
+                <strong>Leaders table for this period</strong>
+                <button onClick={() => sortedList(list, setList, sortValue, setSortValue)}>Sort by</button>
+            </div>
         </>
     )
-}
+} 
