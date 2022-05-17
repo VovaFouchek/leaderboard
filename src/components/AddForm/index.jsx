7958e9fbb64f0cb11b0f5793d8cadff7beb9b824
Volodymyr Fouchek <vinvova555333@gmail.com>
2022-05-17 15:46:58 +0300

    Update List

---
+++ src/components/AddForm/index.jsx
@@ -0,0 +1,46 @@
+import React, { useState } from 'react';
+import { Input, InputLabel, Button } from '@mui/material';
+
+export const AddForm = ({ addList, handleClose }) => {
+  const [values, setValues] = useState({ name: '', score: 0 });
+
+  const handleChange = e => {
+    setValues({
+      ...values,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const onSubmit = e => {
+    e.preventDefault();
+    addList(values);
+    handleClose();
+  };
+  return (
+    <form onSubmit={e => onSubmit(e)}>
+      <InputLabel htmlFor="user-name">Add user name:</InputLabel>
+      <Input
+        onChange={e => handleChange(e)}
+        defaultValue={values.name}
+        name="name"
+        id="user-name"
+        aria-describedby="my-helper-text"
+        sx={{ mb: '30px' }}
+        required
+      />
+      <InputLabel htmlFor="user-score">Add user score:</InputLabel>
+      <Input
+        onChange={e => handleChange(e)}
+        defaultValue={values.score}
+        name="score"
+        type="number"
+        id="user-score"
+        aria-describedby="my-helper-text"
+        required
+      />
+      <Button type="submit" variant="contained" color="success" sx={{ mt: '20px' }}>
+        Success
+      </Button>
+    </form>
+  );
+};
